ee226db4b9b903e998963ae6623c23dec173dcd6
kcvpll <korsun.marketing@gmail.com>
2019-02-04 19:18:17 +0300

    Создание дополнительных компонентов

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ModelComponent } from './model/model.component';
+import { StoreComponent } from './store/store.component';
+import { AdminComponent } from './admin/admin.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ModelComponent,
+    StoreComponent,
+    AdminComponent
   ],
   imports: [
     BrowserModule,
