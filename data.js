e7a6e242b62037a0ba1499c8f1dffc2f1fa8119c
kcvpll <korsun.marketing@gmail.com>
2019-02-04 18:18:06 +0300

    Добавление data.js с данными товаров

---
+++ data.js
@@ -0,0 +1,17 @@
+module.exports = function (){
+    return{
+        products: [
+            {id: 1, name: "Kayak", category: "Watersports", decription: "A boat for one person", price: "10"},
+            {id: 2, name: "Kayak", category: "Watersports", decription: "A boat for one person", price: "11"},
+            {id: 3, name: "Kayak", category: "Watersports", decription: "A boat for one person", price: "12"},
+            {id: 4, name: "Kayak", category: "Watersports", decription: "A boat for one person", price: "13"},
+            {id: 5, name: "Kayak", category: "Watersports", decription: "A boat for one person", price: "14"},
+            {id: 6, name: "Kayak", category: "Watersports", decription: "A boat for one person", price: "15"},
+            {id: 7, name: "Kayak", category: "Watersports", decription: "A boat for one person", price: "16"},
+            {id: 8, name: "Kayak", category: "Watersports", decription: "A boat for one person", price: "17"},
+            {id: 9, name: "Kayak", category: "Watersports", decription: "A boat for one person", price: "18"},
+        ],
+        orders: []
+    }
+    
+}
